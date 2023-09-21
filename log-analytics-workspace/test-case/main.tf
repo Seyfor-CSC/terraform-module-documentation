a358e17037bf821589b2f2c42a6133f748357843
seyfor-internal <62bdef97.solitea.cz@emea.teams.ms>
2023-09-21 08:43:52 +0000

    Release new log-analytics-workspace module version [Release v1.3.0]

--- log-analytics-workspace/test-case/main.tf
+++ log-analytics-workspace/test-case/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.67.0"
+      version = "=3.73.0"
     }
   }
   backend "local" {}
@@ -33,7 +33,7 @@ resource "azurerm_log_analytics_workspace" "la" {
 }
 # log analytics workspace
 module "log_analytics_workspace" {
-  source = "git@github.com:Seyfor-CSC/mit.log-analytics-workspace.git?ref=v1.2.0"
+  source = "git@github.com:Seyfor-CSC/mit.log-analytics-workspace.git?ref=v1.3.0"
   config = local.la
 
   depends_on = [
